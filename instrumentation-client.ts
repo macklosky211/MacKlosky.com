3d1fb142ce9ae75cd097985fa84cebd67686c993
Mac Klosky <macklosky21@gmail.com>
2026-02-13 16:46:32 -0500

    Attempt fix for posthog init.

--- instrumentation-client.ts
+++ instrumentation-client.ts
@@ -1,9 +1,10 @@
 import posthog from "posthog-js"
 
 posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
-  api_host: "/ingest",
+  api_host: "https://macklosky.com/ingest",
   ui_host: "https://us.posthog.com",
-  defaults: '2025-11-30',
+  person_profiles: "always",
+  defaults: "2025-11-30",
   capture_exceptions: true,
   debug: process.env.NODE_ENV === "development",
 });
